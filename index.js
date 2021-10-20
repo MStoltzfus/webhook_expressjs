ec2f32e18e0706d026cbd55cca8c382f0bcb948c
Michael Stoltzfus <coding@mstoltzfus.com>
2021-10-19 20:51:12 -0400

    refactored to remove deprecated body-parser | the code magically started working too, so thats a plus

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3050;
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(express.json()); //Used to parse JSON bodies
+var urlencodedParser = (express.urlencoded({ extended: true }))//Parse URL-encoded bodies
 
 //app.use( express.json() );
 //app.use(express.urlencoded()); //Parse URL-encoded bodies
@@ -18,9 +18,12 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 //  res.status(200).end()
 //});
 
+const doStuff = (foo) => (console.log("stuff happened"), console.log(foo));
+
 app.post('/webhook', urlencodedParser, function (req, res) {
-  console.log('Webhook triggered:', req.body);
-  res.status(200).end()
+  let foo = req.body;
+  doStuff(foo)
+  res.status(200).end();
 });
 
 app.listen(
