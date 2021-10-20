917c6573f9927eadd8b0454174aa9c6b00d74a3c
Michael S <contact@mstoltzfus.com>
2021-10-20 15:01:16 -0400

    added moment.js to inject formatted timestamp into console with webhook data

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const localtunnel = require('localtunnel');
+const moment = require('moment');
 const app = express();
 const PORT = 3050;
-const localtunnel = require('localtunnel');
 
 app.use(express.json()); //Used to parse JSON bodies
 var urlencodedParser = (express.urlencoded({ extended: true }))//Parse URL-encoded bodies
 
-const doStuff = (foo, origin) => (console.log("The Webhook was Triggered!", origin), console.log(foo));
+const doStuff = (body, origin) => {
+  console.log("The Webhook was Triggered!", origin);
+  console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
+  console.log(body);
+};
 const openTunnel = async (PORT) => {
   const tunnel = await localtunnel({ port: PORT });
   console.log('Public tunnel live on ' + tunnel.url);
@@ -14,9 +19,9 @@ const openTunnel = async (PORT) => {
 
 //creates a "/webhook endpoint to the domain that can process post requests and console.logs the results"
 app.post('/webhook', urlencodedParser, function (req, res) {
-  let foo = req.body;
+  let body = req.body;
   let origin = '/webhook';
-  doStuff(foo, origin);
+  doStuff(body, origin);
   res.send("POST Request Recieved! Pretty neat, ain't it!? You sent it to " + origin + ", right?");
   res.status(200).end();
 });
@@ -24,9 +29,9 @@ app.post('/webhook', urlencodedParser, function (req, res) {
 //creates a "/" endpoint to the domain
 
 app.post('/', urlencodedParser, function (req, res) {
-  let foo = req.body;
+  let body = req.body;
   let origin = '/';
-  doStuff(foo, origin);
+  doStuff(body, origin);
   res.send("POST Request Recieved from " + origin);
   res.status(200).end();
 });
