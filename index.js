c6000d20e391e74cc8534ab55ad2b3aa4f2a5fac
Michael Stoltzfus <coding@mstoltzfus.com>
2021-10-19 21:08:04 -0400

    trying to add "/" endpoint

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ const doStuff = (foo) => (console.log("The Webhook was Triggered!"), console.log
 //creates a "/webhook endpoint to the domain that can process post requests and console.logs the results"
 app.post('/webhook', urlencodedParser, function (req, res) {
   let foo = req.body;
-  doStuff(foo)
+  doStuff(foo);
+  res.send("POST Request Recieved! Pretty neat, ain't it!?");
+  res.status(200).end();
+});
+
+//creates a "/" endpoint to the domain
+app.post('/', urlencodedParser, function (req, res) {
+  let foo = req.body;
+  doStuff(foo);
+  res.send("POST Request Recieved");
   res.status(200).end();
 });
 
