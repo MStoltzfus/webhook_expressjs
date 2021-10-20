c97b03e7e8e65c372243e8369a4df7fa6dbe6685
Michael Stoltzfus <coding@mstoltzfus.com>
2021-10-19 21:14:17 -0400

    added some additional response messages

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ var urlencodedParser = (express.urlencoded({ extended: true }))//Parse URL-encod
 
 //Make sure you run "npm install -g localtunnel" (unless you already have localtunnel installed) and run "lt --port 3050" to make the webhook endpoint publicly available!
 
-const doStuff = (foo) => (console.log("The Webhook was Triggered!"), console.log(foo));
+const doStuff = (foo, origin) => (console.log("The Webhook was Triggered!", origin), console.log(foo));
 
 //creates a "/webhook endpoint to the domain that can process post requests and console.logs the results"
 app.post('/webhook', urlencodedParser, function (req, res) {
   let foo = req.body;
-  doStuff(foo);
-  res.send("POST Request Recieved! Pretty neat, ain't it!?");
+  let origin = '/webhook';
+  doStuff(foo, origin);
+  res.send("POST Request Recieved! Pretty neat, ain't it!? You sent it to " + origin + ", right?");
   res.status(200).end();
 });
 
 //creates a "/" endpoint to the domain
 app.post('/', urlencodedParser, function (req, res) {
   let foo = req.body;
-  doStuff(foo);
-  res.send("POST Request Recieved");
+  let origin = ' from /';
+  doStuff(foo, origin);
+  res.send("POST Request Recieved from " + origin);
   res.status(200).end();
 });
 
